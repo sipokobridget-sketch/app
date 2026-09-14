99cbbbbabb9e611d38c25aa45dfa85e3bfa34e25
sipokobridget-sketch <sipokobridget@gmail.com>
2026-09-14 11:18:11 +0200

    fixed code

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
-import { Button } from 'react-native';
-import { SectionList, } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World, i can code on all phones!</Text>
+      <Text style={styles.welcomeText}>Hello World, i can code on all phones!</Text>
+      <Text style={styles.welcomeText}>I found the App.tsx and my name is Bridget</Text>
+      <Text style={styles.welcomeText}>Hellp world, I can code now</Text>
+
+      <View style={styles.formContainer}>
+        <Text>Welcome to my app!</Text>
+        <Text>Enter Name: </Text>
+        <TextInput placeholder="Enter name" />
+        <Text>Enter Surname:</Text>
+        <TextInput placeholder="surname" />
+        <Button title="Add User" />
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -18,31 +28,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 24,
   },
-});
-<View>
-  <Text>Welcome to my app!</Text>
-  <Text>Enter Name: </Text>
-  <TextInput placeholder="Enter name" />
-  <Text> Enter Surname:</Text>
-  <TextInput placeholder=" surname" />
-  <Button title="Add User" />
-</View>
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>I found the App.tsx and my name is Bridget</Text>
-
-      <Text> Hellp world, I can code now </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
+  formContainer: {
+    width: '100%',
+    maxWidth: 320,
+    marginTop: 20,
+  },
   welcomeText: {
-    paddingTop: 40,
+    paddingTop: 10,
     color: 'purple',
     fontWeight: 'bold',
     fontSize: 28,
