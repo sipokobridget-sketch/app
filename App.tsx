4831069b9630165bc1927a3a19b2e9748d18231c
sipokobridget-sketch <sipokobridget@gmail.com>
2026-09-14 11:08:45 +0200

    first code

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button } from 'react-native';
+import { SectionList, } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello World, i can code on all phones!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+});
+<View>
+  <Text>Welcome to my app!</Text>
+  <Text>Enter Name: </Text>
+  <TextInput placeholder="Enter name" />
+  <Text> Enter Surname:</Text>
+  <TextInput placeholder=" surname" />
+  <Button title="Add User" />
+</View>
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>I found the App.tsx and my name is Bridget</Text>
+
+      <Text> Hellp world, I can code now </Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  welcomeText: {
+    paddingTop: 40,
+    color: 'purple',
+    fontWeight: 'bold',
+    fontSize: 28,
+    textAlign: 'center',
   },
 });
